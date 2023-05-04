6bce911fb6aafb86e986eae425e7b5dddbe6fa2e
Shivam Singh <125348423+shivamknoldus1869@users.noreply.github.com>
2023-05-04 12:50:58 +0530

    Update SearchStepDefinition.java

--- Validate Elements On An Amazon/src/test/java/stepDefinition/SearchStepDefinition.java
+++ Validate Elements On An Amazon/src/test/java/stepDefinition/SearchStepDefinition.java
@@ -26,15 +26,15 @@ public class SearchStepDefinition {
         Assert.assertTrue(tcs.pom.getSearchPage().openProductPage());
     }
     @Then("I should see the Buy Now button")
-    public void iShouldSeeTheBuyNowButton() {
+    public void iShouldSeeTheBuyNowButton() throws IOException  {
         Assert.assertTrue(tcs.pom.getSearchPage().verifyByNowButton());
     }
     @Then("I should check if customer rating for the product is over four")
-    public void i_should_check_if_customer_rating_for_the_product_is_over_four() {
+    public void i_should_check_if_customer_rating_for_the_product_is_over_four() throws IOException  {
         Assert.assertTrue(tcs.pom.getSearchPage().verifyRating());
     }
     @Then("Print all the offers available on the description page for the product")
-    public void print_all_the_offers_available_on_the_description_page_for_the_product() {
+    public void print_all_the_offers_available_on_the_description_page_for_the_product() throws IOException  {
         Assert.assertTrue(tcs.pom.getSearchPage().printDescription());
     }
 
