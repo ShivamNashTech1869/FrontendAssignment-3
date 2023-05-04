eead99f02b2d6c4c6478bd153c324105bcd36c9f
Shivam Singh <125348423+shivamknoldus1869@users.noreply.github.com>
2023-05-04 13:14:25 +0530

    Update SearchStepDefinition.java

--- Validate Elements On An Amazon/src/test/java/stepDefinition/SearchStepDefinition.java
+++ Validate Elements On An Amazon/src/test/java/stepDefinition/SearchStepDefinition.java
@@ -5,7 +5,7 @@ import util.TestContextSetup;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
+import java.io.IOException;
 
 public class SearchStepDefinition {
 
